ea80e62e8d1f07c87370c5cbe72407a110352432
ZofiaDrabek <zosia.drabek@gmail.com>
2023-05-25 15:06:04 +0200

    Initial Commit

---
+++ Thalia/ThaliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThaliaApp.swift
+//  Thalia
+//
+//  Created by Zofia Drabek on 25.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ThaliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
